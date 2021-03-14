508152d7a7af00c5374dc64e6d498ea359a7c125
ricardocanales00 <34326913+ricardocanales00@users.noreply.github.com>
2021-03-14 17:07:40 -0600

    Include proyect A solution

---
+++ proyA/proyA.cpp
@@ -0,0 +1,38 @@
+/* 
+ProyA - Median
+Ricardo López Canales A01422699
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+float mean(vector<int> nums){
+	int iTop, iBtm, medium;
+	iTop = nums.size();
+	iBtm = 0;
+	bool 
+	while(){
+		medium = (iTop - iBtm)/2;
+	}
+}
+
+int main(){
+	int iCases, iSize, iHelper;
+	cin>> iCases;
+
+	vector<int> iNums;
+
+	for (int i = 0; i < iCases; ++i)
+	{
+		cin>> iSize;
+		for (int i = 0; i < iSize; ++i)
+		{
+			cin>>iHelper;
+			iNums.push_back(iHelper);
+		}
+
+
+	}
+}
